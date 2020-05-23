eaffd738d99b9f3c36b045b8ce4013138a43bf01
Pedro <hernansaldana52@gmail.com>
2020-05-22 20:10:02 -0500

    Blog dinamico con filtro de categorias

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     
-    path ('', views.blog, name="blog"),
+    path('', views.blog, name="blog"),
+    path('category/<int:category_id>/', views.category, name="category"),
 ]
