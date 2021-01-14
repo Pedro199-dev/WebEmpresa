16d7c18b2e974abb6665123bba343d0447b256a9
Pedro <hernansaldana52@gmail.com>
2021-01-14 17:33:03 -0600

    correcciones

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '13uaosqc83gxfy5b1z!#trqm!pi^1e
 
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['sitesname.com']
 
 
 # Application definition
